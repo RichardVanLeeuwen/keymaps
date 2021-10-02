3f3fc0937c0b47fd44edc6c75d9c0bda6c46d9b4
Richard van Leeuwen <richard.van.leeuwen@ordina.nl>
2021-10-02 11:53:51 +0200

    Finished moving to miryoku

--- DrezaemLeft/keymap.c
+++ DrezaemLeft/keymap.c
@@ -20,29 +20,35 @@
 #define _DVORAK 0
 #define _QWERTY 1
 #define _HOMEMOD 2
-#define _MOUSE 3
-#define _MACROL 4
+#define _MACROL 3
+#define _MOUSE 4
 #define _NUMBERS 5
 #define _FUNCTION 6
+#define _SYMBOLS 7
+#define _NAVI 8
+#define _MEDIA 9
 
 #define DVORAK TO(_DVORAK)
 #define QWERTY TO(_QWERTY)
 #define HOMEMOD TG(_HOMEMOD)
 #define MACROL MO(_MACROL)
 #define MOUS_SPC LT(_MOUSE, KC_SPC)
-#define FUNCTIO LT(_FUNCTION, KC_ESC)
+#define FUNCTIO LT(_FUNCTION, KC_DEL)
 #define NUMBER LT(_NUMBERS, KC_TAB)
+#define MEDIA LT(_MEDIA, KC_ENT)
+#define NAVI LT(_NAVI, KC_ESC)
+#define SYMBOLS LT(_SYMBOLS, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT_5x6(
       KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLS,
       KC_TAB ,KC_QUOT,KC_COMM,KC_DOT , KC_P  , KC_Y  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_SLSH,
-      MACROL , UI_A  , ALT_O , SFT_E , CTL_U , KC_I  ,                         KC_D  , CTL_H , SHI_T , ALT_N , GUI_S ,KC_MINS,
+      MACROL ,GUI_A  , ALT_O , SFT_E , CTL_U , KC_I  ,                         KC_D  , CTL_H , SHI_T , ALT_N , GUI_S ,KC_MINS,
       KC_LSFT,KC_SCLN, KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_EQL ,
                       KC_LEFT,KC_RGHT,                                                          KC_UP ,KC_DOWN,
-                                      FUNCTIO ,MOUS_SPC,                       KC_LSFT,NUMBER ,
-                                      KC_LCTL,KC_ENT ,                        KC_BSPC,KC_DEL ,
+                                      NAVI   ,MOUS_SPC,                       FUNCTIO,NUMBER ,
+                                      KC_LCTL,MEDIA  ,                        SYMBOLS,KC_DEL ,
                                       HOMEMOD,QWERTY ,                        KC_LGUI,KC_LALT
   ),
 
@@ -51,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
       MACROL , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
       KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                     KC_LEFT,KC_RGHT,                                                         KC_UP ,KC_DOWN,
-                                      RAISE  ,KC_SPC ,                        KC_LSFT,LOWER  ,
+                      KC_LEFT,KC_RGHT,                                                         KC_UP ,KC_DOWN,
+                                      NAVI   ,KC_SPC ,                        KC_LSFT,NUMBER ,
                                       KC_LCTL,KC_ENT ,                        KC_BSPC,KC_DEL ,
                                       KC_HOME,DVORAK ,                        KC_LGUI,KC_LALT
   ),
@@ -60,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_HOMEMOD] = LAYOUT_5x6(
       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-      _______,KC_QUOT,KC_COMM,KC_DOT , KC_P  ,_______,                        _______, KC_H  , KC_T  , KC_N  , KC_S  ,_______,
+      _______, KC_A  , KC_O  , KC_E  , KC_U  ,_______,                        _______, KC_H  , KC_T  , KC_N  , KC_S  ,_______,
       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
                       _______,_______,                                                        _______, _______,
                                       _______,KC_SPC ,                       _______,_______,
@@ -92,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUMBERS] = LAYOUT_5x6(
       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,_______,
-      _______,_______,_______,_______,_______,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,_______,_______,
-      RESET  ,_______,_______,_______,_______,KC_LCBR,                        KC_RCBR, KC_P1 , KC_P2 , KC_P3 ,_______,_______,
-                      _______,_______,                                                         KC_P0 , KC_P0,
+      _______,_______,_______,_______,_______,_______,                        KC_PAST, KC_7  , KC_8  , KC_9  ,KC_PLUS,_______,
+      _______,_______,_______,_______,_______,_______,                        KC_PSLS, KC_4  , KC_5  , KC_6  ,KC_MINS,_______,
+      RESET  ,_______,_______,_______,_______,_______,                         KC_0  , KC_1  , KC_2  , KC_3  ,KC_PEQL,RESET  ,
+                      _______,_______,                                                        _______,_______,
                                       _______,_______,                        _______,_______,
                                       _______,_______,                        _______,_______,
                                       _______,_______,                        _______,_______
@@ -104,12 +110,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FUNCTION] = LAYOUT_5x6(
       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                      KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-      _______,_______,_______,KC_UP  ,_______,KC_LBRC,                      KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-      _______,_______,KC_LEFT,KC_DOWN,KC_RGHT,KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-      RESET  ,_______,C(KC_X),C(KC_C),C(KC_V),KC_LCBR,                      KC_RCBR,_______,_______,_______,_______,KC_VOLD,
-                      _______,_______,                                                      KC_EQL ,_______,
+      _______,_______,_______,_______,_______,_______,                      _______, KC_F1 , KC_F2 , KC_F3 , KC_F4 ,_______,
+      _______,_______,_______,_______,_______,_______,                      _______, KC_F5 , KC_F6 , KC_F7 , KC_F8 ,_______,
+      RESET  ,_______,_______,_______,_______,_______,                      _______, KC_F9 , KC_F10, KC_F11, KC_F12,_______,
+                      _______,_______,                                                      _______,_______,
                                       _______,_______,                      _______,_______,
                                       _______,_______,                      _______,_______,
                                       _______,_______,                      _______,_______
   ),
+
+  [_SYMBOLS] = LAYOUT_5x6(
+      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,KC_GRV ,KC_MINS,KC_EQL ,KC_EXLM,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,KC_BSLS,KC_SLSH,KC_LBRC,KC_RBRC,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,_______,_______,KC_LPRN,KC_RPRN,_______,                        _______,_______,_______,_______,_______,_______,
+                      _______,_______,                                                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______
+  ),
+
+  [_NAVI] = LAYOUT_5x6(
+      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,_______,_______, KC_UP ,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,                        _______,_______,_______,_______,_______,_______,
+      RESET  ,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,_______,                        _______,_______,_______,_______,_______,_______,
+                      C(KC_C),C(KC_V),                                                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______
+  ),
+
+  [_MEDIA] = LAYOUT_5x6(
+      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,KC_VOLU,KC_MUTE,KC_INS ,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,KC_VOLD,KC_MPRV,KC_MPLY,KC_MNXT,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,KC_PAUS,KC_SLCK,KC_PSCR,KC_CAPS,_______,                        _______,_______,_______,_______,_______,_______,
+                      _______,_______,                                                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______
+  ),
+
 };
+
+//   [_LAYER] = LAYOUT_5x6(
+//       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+//       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+//       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+//       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+//                       _______,_______,                                                        _______,_______,
+//                                       _______,_______,                        _______,_______,
+//                                       _______,_______,                        _______,_______,
+//                                       _______,_______,                        _______,_______
+//   ),
+
